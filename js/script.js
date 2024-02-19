aa0f98a9a4ae0c4e3bf2aae75cd3ba7bb368b352
ARSH <arshs287@gmail.com>
2024-02-19 19:38:46 +0530

    onScroll Navbar Effect added

--- js/script.js
+++ js/script.js
@@ -2,21 +2,53 @@
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
-
 /*==================== scroll sections active link ====================*/
+
+// let sections = document.querySelectorAll('section');
+// let navLinks = document.querySelectorAll('.navbar a');
+
+// window.addEventListener('scroll', () => {
+//   let fromTop = window.scrollY;
+
+//   sections.forEach((sec) => {
+//     let navLink = document.querySelector(`.navbar a[href="#${sec.id}"]`);
+//     let secTop = sec.offsetTop - 150;
+//     let secHeight = sec.offsetHeight;
+
+//     if (fromTop >= secTop && fromTop < secTop + secHeight) {
+//       navLinks.forEach((link) => {
+//         link.classList.remove('active');
+//       });
+//       navLink.classList.add('active');
+//     }
+//   });
+// });
+
 let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav a');
+let navLinks = document.querySelectorAll('.navbar a');
 
 window.onscroll = () => {
+  sections.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute('id');
 
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach((links) => {
+        links.classList.remove('active');
+        document
+          .querySelector('.navbar a[href*=' + id + ']')
+          .classList.add('active');
+      });
+    }
+  });
 
-    /*==================== sticky navbar ====================*/
+  /*==================== sticky navbar ====================*/
 
-    /*==================== remove toggle icon and navbar when click navbar link (scroll) ====================*/
+  /*==================== remove toggle icon and navbar when click navbar link (scroll) ====================*/
 };
 
-
 /*==================== scroll reveal ====================*/
-
 
 /*==================== typed js ====================*/
